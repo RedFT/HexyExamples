34dd514260641f9c7038b8548f45e2c452525d6b
Norbu Tsering <norbu.tsering.cs@gmail.com>
2019-05-07 20:22:51 -0700

    removed a lot of unused lines. better readability

--- example.py
+++ example.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append("..")
-
 import numpy as np
 import hexy as hx
 import pygame as pg
@@ -10,11 +7,11 @@ from example_hex import make_hex_surface
 
 COL_IDX = np.random.randint(0, 4, (7 ** 3))
 COLORS = np.array([
-    [244, 98, 105],  # red
-    [251, 149, 80],  # orange
+    [244, 98, 105],   # red
+    [251, 149, 80],   # orange
     [141, 207, 104],  # green
-    [53, 111, 163],  # water blue
-    [85, 163, 193],  # sky blue
+    [53, 111, 163],   # water blue
+    [85, 163, 193],   # sky blue
 ])
 
 
@@ -36,20 +33,62 @@ class Selection:
             else:
                 return "point"
 
-
     @staticmethod
     def get_selection(selection_type, cube_mouse, rad, clicked_hex=None):
-            if selection_type == Selection.Type.DISK:
-                return hx.get_disk(cube_mouse, rad)
-            elif selection_type == Selection.Type.RING:
-                return hx.get_ring(cube_mouse, rad)
-            elif selection_type == Selection.Type.LINE:
-                return hx.get_hex_line(clicked_hex, cube_mouse)
-            else:
-                return cube_mouse.copy()
+        if selection_type == Selection.Type.DISK:
+            return hx.get_disk(cube_mouse, rad.value)
+        elif selection_type == Selection.Type.RING:
+            return hx.get_ring(cube_mouse, rad.value)
+        elif selection_type == Selection.Type.LINE:
+            return hx.get_hex_line(clicked_hex, cube_mouse)
+        else:
+            return cube_mouse.copy()
 
 
-class ExampleHexMap():
+class ClampedInteger:
+    """
+    A simple class for "clamping" an integer value between a range. Its value will not increase beyond `upper_limit`
+    and will not decrease below `lower_limit`.
+    """
+    def __init__(self, initial_value, lower_limit, upper_limit):
+        self.value = initial_value
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+
+    def increment(self):
+        self.value += 1
+        if self.value > self.upper_limit:
+            self.value = self.upper_limit
+
+    def decrement(self):
+        self.value -= 1
+        if self.value < self.lower_limit:
+            self.value = self.lower_limit
+
+
+class CyclicInteger:
+    """
+    A simple helper class for "cycling" an integer through a range of values. Its value will be set to `lower_limit`
+    if it increases above `upper_limit`. Its value will be set to `upper_limit` if its value decreases below
+    `lower_limit`.
+    """
+    def __init__(self, initial_value, lower_limit, upper_limit):
+        self.value = initial_value
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+
+    def increment(self):
+        self.value += 1
+        if self.value > self.upper_limit:
+            self.value = self.lower_limit
+
+    def decrement(self):
+        self.value -= 1
+        if self.value < self.lower_limit:
+            self.value = self.upper_limit
+
+
+class ExampleHexMap:
     def __init__(self, size=(600, 600), hex_radius=22, caption="ExampleHexMap"):
         self.caption = caption
         self.size = np.array(size)
@@ -57,13 +96,11 @@ class ExampleHexMap():
         self.center = self.size / 2
 
         self.hex_radius = hex_radius
-        self.hex_apothem = hex_radius * np.sqrt(3) / 2
-        self.hex_offset = np.array([self.hex_radius * np.sqrt(3) / 2, self.hex_radius])
 
         self.hex_map = hx.HexMap()
         self.max_coord = 6
 
-        self.rad = 3
+        self.rad = ClampedInteger(3, 1, 5)
 
         self.selected_hex_image = make_hex_surface(
                 (128, 128, 128, 160), 
@@ -71,26 +108,27 @@ class ExampleHexMap():
                 (255, 255, 255), 
                 hollow=True)
 
-        self.selection_type = 3
+        self.selection_type = CyclicInteger(3, 0, 3)
         self.clicked_hex = np.array([0, 0, 0])
 
         # Get all possible coordinates within `self.max_coord` as radius.
-        coords = hx.get_spiral(np.array((0, 0, 0)), 1, self.max_coord)
+        spiral_coordinates = hx.get_spiral(np.array((0, 0, 0)), 1, self.max_coord)
 
-        # Convert coords to axial coordinates, create hexes and randomly filter out some hexes.
+        # Convert `spiral_coordinates` to axial coordinates, create hexes and randomly filter out some hexes.
         hexes = []
-        num_hexes = np.random.binomial(len(coords), .9)
-        axial_coords = hx.cube_to_axial(coords)
-        axial_coords = axial_coords[np.random.choice(len(axial_coords), num_hexes, replace=False)]
+        num_shown_hexes = np.random.binomial(len(spiral_coordinates), .9)
+        axial_coordinates = hx.cube_to_axial(spiral_coordinates)
+        axial_coordinates = axial_coordinates[np.random.choice(len(axial_coordinates), num_shown_hexes, replace=False)]
 
-        for i, axial in enumerate(axial_coords):
-            colo = list(COLORS[COL_IDX[i]])
-            colo.append(255)
-            hexes.append(ExampleHex(axial, colo, hex_radius))
+        for i, axial in enumerate(axial_coordinates):
+            hex_color = list(COLORS[COL_IDX[i]])
+            hex_color.append(255)
+            hexes.append(ExampleHex(axial, hex_color, hex_radius))
             hexes[-1].set_value(i)  # the number at the center of the hex
 
-        self.hex_map[np.array(axial_coords)] = hexes
+        self.hex_map[np.array(axial_coordinates)] = hexes
 
+        # pygame specific variables
         self.main_surf = None
         self.font = None
         self.clock = None
@@ -110,6 +148,7 @@ class ExampleHexMap():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+
             if event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.clicked_hex = hx.pixel_to_cube(
@@ -117,17 +156,17 @@ class ExampleHexMap():
                             self.hex_radius)
                     self.clicked_hex = self.clicked_hex[0]
                 if event.button == 3:
-                    self.selection_type += 1
+                    self.selection_type.increment()
                 if event.button == 4:
-                    self.rad += 1
+                    self.rad.increment()
                 if event.button == 5:
-                    self.rad -= 1
+                    self.rad.decrement()
 
             if event.type == pg.KEYUP:
                 if event.key == pg.K_UP:
-                    self.rad += 1
+                    self.rad.increment()
                 elif event.key == pg.K_DOWN:
-                    self.rad -= 1
+                    self.rad.decrement()
 
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
@@ -137,13 +176,6 @@ class ExampleHexMap():
 
     def main_loop(self):
         running = self.handle_events()
-        if self.rad > 5:
-            self.rad = 5
-        elif self.rad < 1:
-            self.rad = 1
-
-        if self.selection_type > 3:
-            self.selection_type = 0
 
         return running
 
@@ -151,11 +183,11 @@ class ExampleHexMap():
         # show all hexes
         hexagons = list(self.hex_map.values())
         hex_positions = np.array([hexagon.get_draw_position() for hexagon in hexagons])
-        sorted_idxs = np.argsort(hex_positions[:,1])
-        for idx in sorted_idxs:
-            self.main_surf.blit(hexagons[idx].image, hex_positions[idx] + self.center)
+        sorted_indexes = np.argsort(hex_positions[:, 1])
+        for index in sorted_indexes:
+            self.main_surf.blit(hexagons[index].image, hex_positions[index] + self.center)
 
-        # draw values of hexes
+        # draw numbers on the hexes
         for hexagon in list(self.hex_map.values()):
             text = self.font.render(str(hexagon.value), False, (0, 0, 0))
             text.set_alpha(160)
@@ -167,25 +199,20 @@ class ExampleHexMap():
         cube_mouse = hx.pixel_to_cube(mouse_pos, self.hex_radius)
 
         # choose either ring or disk
-        rad_hex = Selection.get_selection(self.selection_type, cube_mouse, self.rad, self.clicked_hex)
+        rad_hex = Selection.get_selection(self.selection_type.value, cube_mouse, self.rad, self.clicked_hex)
 
         rad_hex_axial = hx.cube_to_axial(rad_hex)
         hexes = self.hex_map[rad_hex_axial]
 
-        list(map(lambda hexagon: 
-                self.main_surf.blit(
-                    self.selected_hex_image, hexagon.get_draw_position() + self.center), 
-                hexes))
+        list(map(self.draw_hex, hexes))
 
-        # draw hud
-        self.selection_type
-
+        # draw "HUD"
         selection_type_text = self.font.render(
-                "(Right Click To Change) Selection Type: " + Selection.Type.to_string(self.selection_type),
+                "(Right Click To Change) Selection Type: " + Selection.Type.to_string(self.selection_type.value),
                 True,
                 (50, 50, 50))
         radius_text = self.font.render(
-                "(Scroll Mouse Wheel To Change) Radius: " + str(self.rad), 
+                "(Scroll Mouse Wheel To Change) Radius: " + str(self.rad.value),
                 True, 
                 (50, 50, 50))
         fps_text = self.font.render(" FPS: " + str(int(self.clock.get_fps())), True, (50, 50, 50))
@@ -193,17 +220,23 @@ class ExampleHexMap():
         self.main_surf.blit(radius_text, (5, 15))
         self.main_surf.blit(selection_type_text, (5, 30))
 
-        # Update screen
+        # Update screen at 30 frames per second
         pg.display.update()
         self.main_surf.fill(COLORS[-1])
         self.clock.tick(30)
 
+    def draw_hex(self, hexagon):
+        self.main_surf.blit(self.selected_hex_image, hexagon.get_draw_position() + self.center)
+
     def quit_app(self):
         pg.quit()
+        raise SystemExit
 
 
 if __name__ == '__main__':
-    ehm = ExampleHexMap()
-    while ehm.main_loop():
-        ehm.draw()
-    ehm.quit_app()
+    example_hex_map = ExampleHexMap()
+
+    while example_hex_map.main_loop():
+        example_hex_map.draw()
+
+    example_hex_map.quit_app()
